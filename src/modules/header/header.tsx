a473bf2f9cba9dd9776f4d869dc6ce76387a1a7f
Max Kasperowicz <kasperowicz.maksymilian@gmail.com>
2022-10-27 17:49:21 +0200

    chore: change project name to J.A.T

--- src/modules/header/header.tsx
+++ src/modules/header/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="grid grid-cols-[3rem,1fr,3rem] h-12 border-b border-primary">
       <LanguagePicker />
       <Nav />
-      <a href='https://github.com/yet3/yata.git' className='border-l border-primary grid place-items-center' target='_blank' rel='noopener noreferrer'>
+      <a href='https://github.com/yet3/J.A.T.git' className='border-l border-primary grid place-items-center' target='_blank' rel='noopener noreferrer'>
         <GithubSvg width={26} />
       </a>
     </header>
